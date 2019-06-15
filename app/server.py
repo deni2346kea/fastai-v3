31a77ca5f8c54743b6efa714eb583b6831d04172
Denis Kutnar <deni2346@stud.kea.dk>
2019-06-15 21:50:40 +0200

    classes

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://www.dropbox.com/s/bqj2587gnjcyf0b/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['NORMAL', 'PNEUMONIA']
+classes = ['PNEUMONIA', 'NORMAL']
 path = Path(__file__).parent
 
 app = Starlette()
